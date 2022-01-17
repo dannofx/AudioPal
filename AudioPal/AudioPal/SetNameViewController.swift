8da3f3bde6bbc2f24d30d2bbf085becefe16a144
Jan Weiß <jan@geheimwerk.de>
2022-01-17 15:29:26 +0100

    Cleanup.

--- AudioPal/AudioPal/SetNameViewController.swift
+++ AudioPal/AudioPal/SetNameViewController.swift
@@ -16,7 +16,7 @@ class SetNameViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var logoContainer: UIView!
     @IBOutlet weak var logoConstraint: NSLayoutConstraint!
     var initialLogoValue: CGFloat = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.isEnabled = false
